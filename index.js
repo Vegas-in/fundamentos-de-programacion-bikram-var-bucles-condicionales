995f2e503c0d6ee19b565966f21910f1279160c5
Vegas-in <angeldevegas@gmail.com>
2024-02-12 23:12:40 +0100

    cambios

---
+++ index.js
@@ -0,0 +1,6 @@
+let variableSinValor = undefined;
+let booleano1 = true; let booleano2 = false;
+const PI = 3.14;
+const TAU = 2*PI;
+let booleanoAnd = booleano1 == booleano2;
+let booleanoNot = PI != booleano1;
